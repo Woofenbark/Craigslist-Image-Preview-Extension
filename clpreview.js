95c9b99fe83a7e87f2e690f56c2204ef69682de9
Woofenbark <ben@woofenbark.com>
2012-07-31 16:26:31 -0700

    Cleaned up js to work with new post DOM.

--- clpreview.js
+++ clpreview.js
@@ -6,25 +6,45 @@ var p_span;
 var imgs;
 var link;
 
-$('span.p').each(function(i) {
-  parent = $(this).parent();
-  link = parent.children('a').attr('href');
+function getImages($row) {
+    /* Get link to actual post */
+    link = $row.children('a').attr('href');
 
-  $.ajax({
-    async: false,
-    type: 'GET',
-    dataType: 'html',
-    url: link,
-    timeout: 1000,
-    success: function(data) {
-      var result = jQuery(data);
-      imgs = result.find('img');
-      imgs.each(function(index) {
-        img = $(this);
-        parent.append('<img src="'+img.attr('src')+'" width="200px" style="margin:2px;"/>');
-      });
-    },
-  });
+    /* AJAX to get the images */
+    $.ajax({
+        async: false,
+        type: 'GET',
+        dataType: 'html',
+        url: link,
+        timeout: 1000,
+        success: function(data) {
+            var result = jQuery(data);
+            if ( 0 == result.find('#iwt').length ) {
+                /* If single image */
+                img = result.find('#iwi').attr('src');
+                $row.append('<img src="'+img+'" width="200px" height="auto" style="margin:2px;"/>');
+            } else {
+                /* If multiple images */
+                imgs = result.find('#iwt > .tn');
+                imgs.each(function(index) {
+                    img = $(this).find('a').attr('href');
+                    $row.append('<img src="'+img+'" width="200px" height="auto" style="margin:2px;"/>');
+                });
+            }
+        },
+    });
+}
 
+/* Get all span.itempx with children
+ * i.e. Not empty, contains images
+ */
+$('span.itempx > *').each(function(i) {
+
+    /* Get ancestor row */
+    parent = $(this).parents('p.row');
+    //parent = $($('span.itempx > *')[0]).parents('p.row');
+    
+    setTimeout(getImages, 0, parent);
+    
 });
 
