08ba4a801480350f7c0a8724973e5705a6ca42ea
Woofenbark <ben@woofenbark.com>
2012-07-31 17:35:48 -0700

    Just working on enhancements

--- clpreview.js
+++ clpreview.js
@@ -6,13 +6,32 @@ var p_span;
 var imgs;
 var link;
 
+function visible($row) {
+    var w = $(window);
+    
+    var windowTop = w.scrollTop();
+    var windowBottom = windowTop + w.height();
+    
+    var rowTop = $row.offset().top;
+    
+    if ( windowTop < rowTop && windowBottom > rowTop ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function appendImage($row, src) {
+    $row.append('<img src="'+src+'" width="200px" height="auto" style="margin:2px;"/>');
+}
+
 function getImages($row) {
     /* Get link to actual post */
     link = $row.children('a').attr('href');
 
     /* AJAX to get the images */
     $.ajax({
-        async: false,
+        async: true,
         type: 'GET',
         dataType: 'html',
         url: link,
@@ -20,15 +39,24 @@ function getImages($row) {
         success: function(data) {
             var result = jQuery(data);
             if ( 0 == result.find('#iwt').length ) {
-                /* If single image */
-                img = result.find('#iwi').attr('src');
-                $row.append('<img src="'+img+'" width="200px" height="auto" style="margin:2px;"/>');
+                if ( 0 == result.find('#iwi').length ) {
+                    /* Inline image(s) */
+                    imgs = result.find('img');
+                    imgs.each(function(index) {
+                        img = $(this).attr('src');
+                        appendImage($row, img);
+                    });
+                } else {
+                    /* If single image */
+                    img = result.find('#iwi').attr('src');
+                    appendImage($row, img);
+                }
             } else {
                 /* If multiple images */
                 imgs = result.find('#iwt > .tn');
                 imgs.each(function(index) {
                     img = $(this).find('a').attr('href');
-                    $row.append('<img src="'+img+'" width="200px" height="auto" style="margin:2px;"/>');
+                    appendImage($row, img);
                 });
             }
         },
@@ -43,8 +71,14 @@ $('span.itempx > *').each(function(i) {
     /* Get ancestor row */
     parent = $(this).parents('p.row');
     //parent = $($('span.itempx > *')[0]).parents('p.row');
-    
-    setTimeout(getImages, 0, parent);
+
+    /* TODO: Refactor visible check,
+     * we don't want to compute window dims every time.
+     * Maybe do 5 at a time??
+     */    
+    //if ( visible(parent) ) {
+        setTimeout(getImages, 0, parent);
+    //}
     
 });
 
